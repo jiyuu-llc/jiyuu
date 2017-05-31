f653797bab19f6f48c6ca79ac45c778489e9b948
Alec Wantoch <alec@wantoch.net>
2017-05-31 15:46:41 -0400

    fixing profile, converted more components

--- client/modules/core/components/profile/profile.jsx
+++ client/modules/core/components/profile/profile.jsx
@@ -4,26 +4,28 @@ import List from '../../containers/profile/list.js';
 import ProfileButtonsBar from './profileButtonsBar.jsx';
 import ProfileSidebar from './profileSidebar.jsx';
 
-const Profile = () => ({
+class Profile extends React.Component {
+    constructor(props) {
+        super(props);
+    }
 
-  render() {
-    return (
-      <div className="container profile-container">
-                <ProfileSidebar/>
-                <div id="profile-filler" className="col-md-3 hidden-sm-down">
-                </div>
-                <div className="col-md-8 profileRight">
-                  <ProfileOverview />
-                  <div id="profileMain" className="col-xs-12">
-                    <ProfileButtonsBar />
-                    <br/>
-                    <List />
-                  </div>
-                </div>
-      </div>
-    );
-  }
-
-});
+    render() {
+        return (
+          <div className="container profile-container">
+                    <ProfileSidebar/>
+                    <div id="profile-filler" className="col-md-3 hidden-sm-down">
+                    </div>
+                    <div className="col-md-8 profileRight">
+                      <ProfileOverview />
+                      <div id="profileMain" className="col-xs-12">
+                        <ProfileButtonsBar />
+                        <br/>
+                        <List />
+                      </div>
+                    </div>
+          </div>
+        )
+    }
+}
 
 export default Profile;
